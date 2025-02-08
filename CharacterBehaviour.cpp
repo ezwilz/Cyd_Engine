e49323e3757c31cdc1a747b98332563e0bc1b5ab
ezwilz <esmemrwilson@gmail.com>
2025-02-08 20:54:37 +0000

    Removed character and moved the contents to characterbehaviour  instead.

--- CharacterBehaviour.cpp
+++ CharacterBehaviour.cpp
@@ -1,1 +1,10 @@
 #include "CharacterBehaviour.h"
+
+
+void CharacterBehaviour::passive()
+{
+	food -= 0.5 * foodMultiplier;
+	bladder -= 0.5 * bladderMultiplier;
+	hygeine -= 0.5 * hygeineMultiplier;
+	sleep -= 0.5 * sleepMultiplier;
+} 
--- CharacterBehaviour.h
+++ CharacterBehaviour.h
@@ -1,5 +1,19 @@
 #pragma once
 class CharacterBehaviour
 {
+private:
+	//Needs
+	float food = 1000;
+	float bladder = 1000;
+	float sleep = 1000;
+	float hygeine = 1000;
+
+public:
+	float foodMultiplier = 1;
+	float bladderMultiplier = 1;
+	float sleepMultiplier = 1;
+	float hygeineMultiplier = 1;
+
+	void passive();
 };
 
